3e38b16765225faf16e139626cd90ec066b0f549
leofeat <aarslanibrahim@gmail.com>
2019-12-23 22:00:48 +0300

    v1
    mobilproje

---
+++ src/components/Converter.js
@@ -0,0 +1,195 @@
+import React, {Component} from 'react';
+import {
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    TouchableOpacity,
+    Alert, 
+    Image,
+    TouchableHighlight
+} from 'react-native';
+import axios from 'axios';
+
+export default class Converter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usd: '',
+            tl: '',
+            cad: '',
+            jpy: '',
+            cup: '',
+            input: '',
+            rates: null
+        };
+        this.calculateCurrency = this.calculateCurrency.bind(this);
+        this.stateClear = this.stateClear.bind(this);
+    }
+
+    componentWillMount = () => {
+      axios.get('http://data.fixer.io/api/latest?access_key=2aaaf5a3cd4143bc54def47f80c91d08&symbols=USD,EUR,TRY,CAD,JPY,CUP')
+        .then(response => response.data)
+        .then(data => {
+            const rates = data.rates;
+            this.setState({
+                rates
+            })
+        })
+    };
+
+    stateClear() {
+        this.setState({
+            usd: '',
+            tl: '',
+            cad: '',
+            jpy: '',
+            cup: '',
+            input: '',
+        })
+    }
+
+    calculateCurrency() {
+        const inpData = parseFloat(this.state.input);
+        const rates = this.state.rates;
+        if (inpData !== NaN && inpData >= 0) {
+            this.setState({
+                usd: (inpData * rates['USD']),
+                tl: (inpData * rates['TRY']),
+                cad: (inpData * rates['CAD']),
+                jpy: (inpData * rates['JPY']),
+                cup: (inpData * rates['CUP']),
+            })
+        } else {
+            Alert.alert(
+                'Mesaj:',
+                'Lütfen Değer Giriniz',
+                [
+                    { text: 'OK', onPress: (this.stateClear) }
+                ]
+            )
+        }
+    }
+    
+
+    render () {
+        const {wrapper, inputStyle, textStyle, submitButton, submitButtonText, clearButton,Euro,Dolar,Tl} = styles;
+        const {usd, tl, cad, jpy, cup, input} = this.state;
+        return (
+            <View style={wrapper}>
+                <View style={{flexDirection: 'row'}}>
+                    {/* INPUT */}
+                    <TextInput
+                        placeholder={'Euro Değerini Giriniz:'}
+                        style={inputStyle}
+                        onChangeText={(text) => this.setState({ input: text })}
+                        value={input}
+                        keyboardType={'numeric'}
+                        autoFocus
+                    />
+
+                    {/* Hesapla Butonu */}
+                    <TouchableOpacity 
+                        style={submitButton} 
+                        activeOpacity={0.7}
+                        onPress={this.calculateCurrency}
+                    >
+                        <Text style={submitButtonText}>Hesapla</Text>
+                    </TouchableOpacity> 
+					
+				
+
+
+
+                    {/* Rakam Yenileme Butonu */}
+                    <TouchableHighlight
+                        onPress={this.stateClear} 
+                        style={clearButton}
+                    >
+                        <Image 
+                            source={require('../images/refresh.png')}
+                            style={{ width: 30, height: 30, marginTop: 5, marginLeft: 8 }}
+                        />
+                    </TouchableHighlight>
+
+                </View> 
+
+            <Text style={textStyle}>USD : { usd }</Text>    
+            <Text style={textStyle}>TRY : { tl }</Text>
+            <Text style={textStyle}>CAD : { cad }</Text>
+            <Text style={textStyle}>JPY : { jpy }</Text>
+            <Text style={textStyle}>CUP : { cup }</Text>
+			
+			
+				 {/* Euro Hesapla Butonu */}
+                    <TouchableOpacity 
+                        style={Euro} 
+                        activeOpacity={0.7}
+                        onPress={this.calculateCurrency}
+                    >
+                        <Text style={inputStyle} >Euro Seçimi Hesapla Butonu</Text>
+                    </TouchableOpacity> 
+					
+					
+					 {/* dolar Hesapla Butonu */}
+                    <TouchableOpacity 
+                        style={Dolar} 
+                        activeOpacity={0.7}
+                        onPress={this.calculateCurrency}
+                    >
+                        <Text style={inputStyle} >Dolar Seçimi Hesapla Butonu</Text>
+                    </TouchableOpacity> 
+					
+					 {/* TL Hesapla Butonu */}
+                    <TouchableOpacity 
+                        style={Tl} 
+                        activeOpacity={0.7}
+                        onPress={this.calculateCurrency}
+                    >
+                        <Text style={inputStyle} >TL Seçimi Hesapla Butonu</Text>
+                    </TouchableOpacity> 
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+		
+		marginTop: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputStyle: {
+
+        width: 200,
+        height: 40,
+        marginBottom: 30,
+        paddingLeft: 5,
+        borderColor: '#788B91',
+        borderWidth: 1,
+        borderRadius: 2,
+    },
+    textStyle: {
+        width: 250,
+        height: 50,
+        fontWeight: 'bold',
+    },
+    submitButton: {
+        backgroundColor: '#000',
+        height: 40,
+        justifyContent: 'center',
+        marginLeft: 5,
+    },
+    submitButtonText: {
+        padding: 10,
+        color: '#fff',
+        fontSize: 16,
+    },
+    clearButton: {
+        backgroundColor: 'transparent',
+        height: 40,
+        width: 40,
+        marginLeft: 5,
+    }
+})
